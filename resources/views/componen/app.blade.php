724a5f317c04dc0e90cf686a490329da2ab62f80
mohfikryansyah <moh.fikryansyah@gmail.com>
2025-10-24 00:43:01 +0800

    css fix

--- resources/views/componen/app.blade.php
+++ resources/views/componen/app.blade.php
@@ -1,27 +1,28 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="{{ asset('build/assets/app-ee1baeca.css') }}" data-navigate-track="reload">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-ee1baeca.css') }}" data-navigate-track="reload">
     <script type="module" src="{{ asset('build/assets/app-b1941ff8.js') }}" data-navigate-track="reload"></script>
-  @vite(['resources/css/app.css','resources/js/app.js'])
-  {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" /> --}}
-  @livewireStyles
-  <title>@yield('titel')</title>
+    {{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-ee1baeca.css') }}" data-navigate-track="reload" />
+    <script type="module" src="{{ asset('build/assets/app-c8ff57e5.js') }}" data-navigate-track="reload"></script>
+    @livewireStyles
+    <title>@yield('titel')</title>
 </head>
+
 <body class="font-popins relative">
-  @include('sweetalert::alert')
-   
-   @include('componen.nav')
-   @include('componen.sidebar')
-   
-   @yield('isi')
-   @livewireScripts
-   @yield('js')
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+    @include('sweetalert::alert')
+
+    @include('componen.nav')
+    @include('componen.sidebar')
+
+    @yield('isi')
+    @livewireScripts
+    @yield('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
 </body>
+
 </html>
-
-
-  

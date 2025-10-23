3cfb3a2748b0a261b0261ae811ff8f3bfeef1c01
mohfikryansyah <moh.fikryansyah@gmail.com>
2025-10-24 01:23:48 +0800

    Refactor password hashing in UserSeeder to use Hash::make for consistency

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,8 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -19,7 +20,7 @@ class UserSeeder extends Seeder
                 'name' => 'admin',
                 'email' => 'admin@bpkh15gorontalo.com',
                 'role' => 'admin',
-                'password' => Password::hash('123adm!N'),
+                'password' => Hash::make('123adm!N'),
                 'id_divisi' => null,
                 'status' => 'offline',
             ],
@@ -27,7 +28,7 @@ class UserSeeder extends Seeder
                 'name' => 'pimpinan',
                 'email' => 'kabalai@bpkh15gorontalo.com',
                 'role' => 'pimpinan',
-                'password' => Password::hash('123adm!N'),
+                'password' => Hash::make('123adm!N'),
                 'id_divisi' => 4,
                 'status' => 'offline',
             
@@ -36,7 +37,7 @@ class UserSeeder extends Seeder
                 'name' => 'Tata Usaha',
                 'email' => 'kasubag_tu@bpkh15gorontalo.com',
                 'role' => 'kepala divisi',
-                'password' => Password::hash('123adm!N'),
+                'password' => Hash::make('123adm!N'),
                 'id_divisi' => 1,
                 'status' => 'offline',
             
@@ -45,7 +46,7 @@ class UserSeeder extends Seeder
                 'name' => 'ISDHL',
                 'email' => 'kasubag_sdhtl@bpkh15gorontalo.com',
                 'role' => 'kepala divisi',
-                'password' => Password::hash('123adm!N'),
+                'password' => Hash::make('123adm!N'),
                 'id_divisi' => 2,
                 'status' => 'offline',
             

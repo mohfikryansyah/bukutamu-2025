d68bceda45393ac799d3829615a651ec7cb19206
mohfikryansyah <moh.fikryansyah@gmail.com>
2025-10-30 13:24:47 +0800

    Comment out unused stylesheet link in app.blade.php

--- resources/views/componen/app.blade.php
+++ resources/views/componen/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('build/assets/app-ee1baeca.css') }}" data-navigate-track="reload">
+    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-ee1baeca.css') }}" data-navigate-track="reload"> --}}
     <script type="module" src="{{ asset('build/assets/app-b1941ff8.js') }}" data-navigate-track="reload"></script>
     {{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
     <link rel="stylesheet" href="{{ asset('build/assets/app-effb8eb2.css') }}" data-navigate-track="reload" />

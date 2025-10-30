78c2fe67277fbe741a729ca8242723a708a7516b
mohfikryansyah <moh.fikryansyah@gmail.com>
2025-10-30 13:26:56 +0800

    Fix stylesheet link in index.blade.php to use the correct asset version

--- resources/views/componen/guest.blade.php
+++ resources/views/componen/guest.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite(['resources/css/app.css','resources/js/app.js'])
+  {{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
+  <link rel="stylesheet" href="{{ asset('build/assets/app-effb8eb2.css') }}" data-navigate-track="reload" />
 </head>
 <body class="bg-emerald-700">
    @yield('isi')
